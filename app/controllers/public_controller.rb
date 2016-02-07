a92a26aece4d492851a503394bc2ad2ae5468a27
Fabiano Brito <fabianopbrito@gmail.com>
2016-02-07 16:06:23 +0000

    projects CRUD linked to public area

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,9 +4,21 @@ class PublicController < ApplicationController
   
   def index
     @skills = Skill.sorted
+    @projects = Project.sorted
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+  
+  def next_proj
+    @project = Project.where("id > ?", params[:id]).first
+    redirect_to(:action => 'show', :id => @project.id)
+  end
+  
+  def prev_proj
+    @project = Project.where("id < ?", params[:id]).last
+    redirect_to(:action => 'show', :id => @project.id)
   end
   
 end
