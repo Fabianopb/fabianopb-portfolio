3df4e438209992d0a54f43b7f8914513ae693847
Fabiano Brito <fabianopbrito@gmail.com>
2016-02-08 13:09:54 +0000

    fixed access to AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do |config|
     
     if Rails.env.production?
+        
         config.fog_credentials = {
             :provider               => 'AWS',
-            :aws_access_key_id      => ENV['FP_AWS_KEY_ID'],
-            :aws_secret_access_key  => ENV['FP_AWS_SECRET']
+            :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+            :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
         }
       
-        config.fog_directory  = ENV["FP_AWS_BUCKET_PROD"]
+        config.fog_directory  = ENV["S3_BUCKET_PRODUCTION"]
         config.storage = :fog
   
     else
