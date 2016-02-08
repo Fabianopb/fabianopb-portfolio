2ff47c30263dbf28bd03fb88fef35e4d97085b4b
Fabiano Brito <fabianopbrito@gmail.com>
2016-02-08 13:07:43 +0000

    config :fog for production and :file for development

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,17 @@
 CarrierWave.configure do |config|
+    
+    if Rails.env.production?
+        config.fog_credentials = {
+            :provider               => 'AWS',
+            :aws_access_key_id      => ENV['FP_AWS_KEY_ID'],
+            :aws_secret_access_key  => ENV['FP_AWS_SECRET']
+        }
+      
+        config.fog_directory  = ENV["FP_AWS_BUCKET_PROD"]
+        config.storage = :fog
   
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
-      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
-    #   :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = ENV["S3_BUCKET_PRODUCTION"]
+    else
+        config.storage = :file
+    end
   
 end
