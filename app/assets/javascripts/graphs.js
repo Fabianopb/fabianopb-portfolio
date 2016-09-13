a52f7c4ccfa1b139e5d42cd4873498354c00da7c
Fabiano Brito <fabianopbrito@gmail.com>
2016-09-13 19:31:27 +0300

    Adds d3.js gem and creates a simple test.

---
+++ app/assets/javascripts/graphs.js
@@ -0,0 +1,25 @@
+(function() {
+
+	window.onload = function() {
+
+		// var margin = {top: 20, right: 20, bottom: 20, left: 20},
+	 //  width = 240 - margin.left - margin.right,
+	 //  height = 240 - margin.top - margin.bottom;
+
+	 //  var svg = d3.select(".test-graph")
+		// 	.append("svg")
+		//   .attr("width", width + margin.left + margin.right)
+		//   .attr("height", height + margin.top + margin.bottom)
+		// 	.append("g")
+		//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		// svg.append("rect")
+		// 	.attr("width", "100%")
+		// 	.attr("height", "100%")
+		// 	.attr("x", 0)
+		// 	.attr("y", 0)
+		// 	.style("fill", "red");
+
+	};
+
+})();
