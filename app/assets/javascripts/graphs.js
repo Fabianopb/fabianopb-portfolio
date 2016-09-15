1a757c648edbd87e83df40423a6e79b8a2f815a4
Fabiano Brito <fabianopbrito@gmail.com>
2016-09-15 20:33:20 +0300

    Draws basic sunburst graph based on the mock data for the skillset.

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,4 +1,4 @@
-var CHARTS = (function() {
+var BARCHART = (function() {
 
 	var triggerSkillsGraph = true;
 	var skillsData;
@@ -116,7 +116,75 @@ var CHARTS = (function() {
 
 })();
 
-var DATASERVICE = (function () {
+var SUNBURST = (function () {
+
+	var drawSunburst = function() {
+
+		var width = 450,
+	    height = 450,
+	    radius = (Math.min(width, height) / 2) - 10;
+
+		var formatNumber = d3.format(",d");
+
+		var x = d3.scale.linear().range([0, 2 * Math.PI]);
+
+		var y = d3.scale.sqrt().range([0, radius]);
+
+		var color = d3.scale.category20c();
+
+		var partition = d3.layout.partition().value(function(d) { return d.size; });
+
+		var arc = d3.svg.arc()
+	    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+	    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+	    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+	    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
+		var svg = d3.select(".sunburst-container").append("svg")
+	    .attr("width", width)
+	    .attr("height", height)
+		  .append("g")
+	    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+		d3.json("/data/sunburst.json", function(error, root) {
+		  if (error) {
+		  	throw error;
+		 	}
+
+		  svg.selectAll("path")
+	      .data(partition.nodes(root))
+		    .enter().append("path")
+	      .attr("d", arc)
+	      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+	      .style("stroke", "#FFF")
+	      .on("click", click)
+		    .append("title")
+	      .text(function(d) { return d.name; });
+		});
+
+		function click(d) {
+		  svg.transition()
+	      .duration(750)
+	      .tween("scale", function() {
+	        var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+            yd = d3.interpolate(y.domain(), [d.y, 1]),
+            yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+	        return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+		    })
+		    .selectAll("path")
+		    .attrTween("d", function(d) { return function() { return arc(d); }; });
+		}
+
+		d3.select(self.frameElement).style("height", height + "px");
+	};
+
+	return {
+		drawSunburst: drawSunburst
+	};
+
+})();
+
+var GETDATA = (function () {
 
 	window.onload = function() {
 
@@ -127,7 +195,8 @@ var DATASERVICE = (function () {
 		  dataType: 'json',
 		  data: "{}", 
 		  success: function (data) {
-		    CHARTS.drawSkills(data);
+		    BARCHART.drawSkills(data);
+		    // SUNBURST.drawSunburst();
 		  },
 			error: function (error) {
 				console.log(error);
