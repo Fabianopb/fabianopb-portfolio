5eabb2051c84e1ea94608d5b1d809e630d8deb31
Fabiano Brito <fabianopbrito@gmail.com>
2016-09-17 20:41:35 +0300

    Generates JSON template from the app database on specific skills.

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,14 @@
 class DataController < ApplicationController
+	
 	def get_data
 		skills = Skill.sorted
 		render :json => skills.to_json(:only => [:skill_name, :level]), :callback => params[:callback]
   end
+
+  def sunburst
+  	@business_skills = SpecificSkill.filter_category('Business')
+  	@design_skills = SpecificSkill.filter_category('Design')
+  	@technology_skills = SpecificSkill.filter_category('Technology')
+  end
+
 end
--- app/models/specific_skill.rb
+++ app/models/specific_skill.rb
@@ -1,5 +1,6 @@
 class SpecificSkill < ActiveRecord::Base
 
 	scope :sorted, lambda { order("specific_skills.category ASC") }
+	scope :filter_category, ->(category) { where(["category = ?", category]) }
 
 end
