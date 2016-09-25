5df9f2fb640c08930afbd87355b1191bd811b928
Fabiano Brito <fabianopbrito@gmail.com>
2016-09-25 15:11:03 +0300

    Sets up badges controller.

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,13 +1,52 @@
 class BadgesController < ApplicationController
+  
+  layout 'admin'
+  
+  before_action :confirm_logged_in
+  
   def index
+    @badges = Badge.sorted
   end
 
   def new
+    @badge = Badge.new
+  end
+  
+  def create
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @badge = Badge.find(params[:id])
+  end
+  
+  def update
+    @badge = Badge.find(params[:id])
+    if @badge.update_attributes(badge_params)
+      redirect_to(:action => 'index')
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @badge = Badge.find(params[:id])
   end
+  
+  def destroy
+    Badge.find(params[:id]).destroy
+    redirect_to(:action => 'index')
+  end
+  
+  private
+  
+	  def badge_params
+	    params.require(:badge).permit(:name, :image_url, :link)
+	  end
+
 end
