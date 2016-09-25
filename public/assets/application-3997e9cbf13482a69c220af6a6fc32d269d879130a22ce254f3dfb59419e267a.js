b5c86cdc7ea27e2476d5fdd676e3e80474c992b8
Fabiano Brito <fabianopbrito@gmail.com>
2016-09-25 15:57:12 +0300

    Cleans and precompiles assets prior deployment.

--- public/assets/application-3997e9cbf13482a69c220af6a6fc32d269d879130a22ce254f3dfb59419e267a.js
+++ public/assets/application-3997e9cbf13482a69c220af6a6fc32d269d879130a22ce254f3dfb59419e267a.js
@@ -21118,19 +21118,19 @@ return jQuery;
 
 
 }).call(this);
-(function() {
-
-
-}).call(this);
-(function() {
-
-
-}).call(this);
-var CHARTS = (function() {
+var BARCHART = (function() {
+	'use strict';
 
 	var triggerSkillsGraph = true;
 	var skillsData;
 	var skillsTransition;
+
+	var mouseOverEvent = function() {
+		if(triggerSkillsGraph) {
+			triggerSkillsGraph = false;
+			skillsTransition(skillsData);
+		}
+	};
 	
 	var drawSkills = function(data) {
 
@@ -21157,7 +21157,8 @@ var CHARTS = (function() {
 		  .attr("width", width)
 		  .attr("height", height + marginTop)
 			.append("g")
-			.attr("transform", "translate(0," + marginTop + ")");
+			.attr("transform", "translate(0," + marginTop + ")")
+			.on("mouseover", mouseOverEvent);
 
 		svg.selectAll(".skill-scale")
 	  	.data(['BEGINNER', 'PROFICIENT', 'EXPERT'])
@@ -21230,21 +21231,152 @@ var CHARTS = (function() {
 
 	};
 
-	var scrollTrigger = function(e) {
-	  if(triggerSkillsGraph && window.pageYOffset > 1200) {
-	  	skillsTransition(skillsData);
-	  	triggerSkillsGraph = false;
-	  }
-	};
-
 	return {
-		drawSkills: drawSkills,
-		scrollTrigger: scrollTrigger
+		drawSkills: drawSkills
 	};
 
 })();
 
-var DATASERVICE = (function () {
+var SUNBURST = (function () {
+	'use strict';
+
+	var drawSunburst = function(data) {
+
+		var width = 350,
+	    height = 350,
+	    radius = (Math.min(width, height) / 2) - 10;
+
+		var x = d3.scale.linear().range([0, 2 * Math.PI]);
+
+		var y = d3.scale.sqrt().range([0, radius]);
+
+		var hslColors = [[209, 43, 28], [0, 99, 64], [174, 100, 29]];
+
+		var partition = d3.layout.partition().value(function(d) { return d.size; });
+
+		var arc = d3.svg.arc()
+	    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+	    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+	    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+	    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
+		var svg = d3.select(".sunburst-container").append("svg")
+	    .attr("width", width)
+	    .attr("height", height)
+		  .append("g")
+	    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+	  var updateGraphLegend = function(children) {
+
+	  	d3.select(".legend-container").selectAll("*").remove();
+
+	  	if(children) {
+	  		var lineHeight = 40;
+	  		var columns = 2;
+	  		var blockSide = 16;
+	  		var legendHeight = Math.ceil(children.length / columns) * lineHeight;
+
+		  	var legendSvg = d3.select(".legend-container").append("svg")
+			    .attr("width", width)
+			    .attr("height", legendHeight)
+				  .append("g");
+
+				var lineIndex = -1;
+				legendSvg.selectAll("rect")
+					.data(children)
+	    		.enter().append("rect")
+					.attr("width", blockSide)
+					.attr("height", blockSide)
+					.attr("x", function(d) {
+						lineIndex++;
+						lineIndex = lineIndex === columns ? 0 : lineIndex;
+						return lineIndex * width / columns;
+					})
+					.attr("y", function(d, i) { return Math.floor((i) / columns) * lineHeight; })
+					.style("fill", function(d) { return d.color; });
+
+				lineIndex = -1;
+				legendSvg.selectAll(".legend-text")
+			  	.data(children)
+			  	.enter().append("text")
+			    .attr("class", "legend-text")
+		      .attr("x", function(d) {
+						lineIndex++;
+						lineIndex = lineIndex === columns ? 0 : lineIndex;
+						return (blockSide + 5) + lineIndex * width / columns;
+					})
+		      .attr("y", function(d, i) { return Math.floor((i) / columns) * lineHeight; })
+		      .attr("dy", "1em")
+		      .text(function(d) { return d.name; });
+	  	}
+
+	  };
+
+	  var click = function(node) {
+		  svg.transition()
+	      .duration(750)
+	      .tween("scale", function() {
+	        var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]),
+            yd = d3.interpolate(y.domain(), [node.y, 1]),
+            yr = d3.interpolate(y.range(), [node.y ? 20 : 0, radius]);
+	        return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+		    })
+		    .selectAll("path")
+		    .style("opacity", function(d) {
+		    	if(node.children) {
+		    		return node.children.indexOf(d) > -1 ? 1 : 0.15;
+		    	} else {
+		    		return node === d ? 1 : 0.15;
+		    	}
+		    })
+		    .attrTween("d", function(d) { return function() { return arc(d); }; });
+
+		  updateGraphLegend(node.children);
+		};
+
+	 	var nodes = partition.nodes(data);
+
+	 	var colorIndex = 0;
+	 	for (var i = 0, w = nodes.length; i < w; i++) {
+	 		if (nodes[i].depth === 0) {
+	 			nodes[i].color = "transparent";
+	 		}
+	 		else if (nodes[i].depth === 1) {
+	 			var hsl = hslColors[colorIndex];
+	 			nodes[i].color = "hsl(" + hsl[0] + "," + hsl[1] + "%," + hsl[2] + "%)";
+	 			for (var j = 0, z = nodes[i].children.length; j < z; j++) {
+	 				var lighting = 30 + 60 * (j / (z - 1));
+	 				nodes[i].children[j].color = "hsl(" + hsl[0] + "," + hsl[1] + "%," + lighting + "%)";
+	 			}
+	 			colorIndex++;
+	 		}
+	 	}
+
+	  svg.selectAll("path")
+      .data(nodes)
+	    .enter().append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return d.color; })
+      .style("stroke", "#FFF")
+      .style("opacity", function(d) {
+      	return d.depth === 1 ? 1 : 0.15;
+      })
+      .on("click", click)
+	    .append("title")
+      .text(function(d) { return d.name; });
+
+    updateGraphLegend(nodes[0].children);
+
+	};
+
+	return {
+		drawSunburst: drawSunburst
+	};
+
+})();
+
+var GETDATA = (function () {
+	'use strict';
 
 	window.onload = function() {
 
@@ -21255,8 +21387,21 @@ var DATASERVICE = (function () {
 		  dataType: 'json',
 		  data: "{}", 
 		  success: function (data) {
-		    CHARTS.drawSkills(data);
-		    window.onscroll = CHARTS.scrollTrigger;
+		    BARCHART.drawSkills(data);
+		  },
+			error: function (error) {
+				console.log(error);
+		  }
+		});
+
+		$.ajax({
+		  type: "GET",
+		  contentType: "application/json; charset=utf-8",
+		  url: 'data/sunburst',
+		  dataType: 'json',
+		  data: "{}", 
+		  success: function (data) {
+		    SUNBURST.drawSunburst(data);
 		  },
 			error: function (error) {
 				console.log(error);
@@ -21267,10 +21412,6 @@ var DATASERVICE = (function () {
 
 })();
 (function() {
-
-
-}).call(this);
-(function() {
   $(function() {
     return $('a[href^="/#"]').on('click.smoothscroll', function(e) {
       var $target, target;
@@ -21278,16 +21419,12 @@ var DATASERVICE = (function () {
       target = this.hash;
       $target = $(target);
       return $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-      }, 500, 'swing', function() {
+        'scrollTop': $target.offset().top - 200
+      }, 2500, 'swing', function() {
         return window.location.hash = target;
       });
     });
   });
-
-}).call(this);
-(function() {
-
 
 }).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
