ee60e2d4d55f7f733e0817f2259fe5c07c6e39ac
Fabiano Brito <fabianopbrito@gmail.com>
2016-09-25 15:50:03 +0300

    Styles CodeSchool badges area in the public index.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,7 +3,7 @@ class PublicController < ApplicationController
   layout 'public'
   
   def index
-    @skills = Skill.sorted
+    @badges = Badge.sorted
     @projects = Project.sorted
     @info = BasicInfo.sorted.first
   end
